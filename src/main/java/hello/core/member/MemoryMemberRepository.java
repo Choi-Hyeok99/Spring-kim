3e99016c82bc4b8208553b5454da7938fcc46ac9
Choi-Hyeok99 <gur0709@naver.com>
2024-01-17 16:17:39 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long , Member > stroe = new HashMap<>();
+    @Override
+    public void save(Member member) {
+
+        stroe.put(member.getId(),member);
+
+
+    }
+
+    @Override
+    public Member findById(Long meberId) {
+        return stroe.get(meberId);
+    }
+}
