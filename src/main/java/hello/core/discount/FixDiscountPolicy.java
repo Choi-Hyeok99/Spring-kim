3c1b381778f6c72849f1510c747a564fb6686636
Choi-Hyeok99 <gur0709@naver.com>
2024-01-22 17:05:21 +0900

    파일명 수정 , order 추가 ( 미완성 )

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy{
+public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인 ( 고정 할인 )
 
@@ -11,7 +11,7 @@ public class FixDiscountPolicy implements DiscountPolicy{
     public int discount (Member member , int price){
         if (member.getGrade() == Grade.VIP){
             return discountFixAmount;
-        } else{
+        } else {
             return 0;
         }
     }
