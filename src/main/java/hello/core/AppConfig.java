e91170463f552b3aceb7df4e50d7958364ac2077
Choi-Hyeok99 <gur0709@naver.com>
2024-01-23 17:22:51 +0900

    %할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -21,7 +21,7 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(),discountPolicy()); // 생성자 주입
         }
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
     }
 
