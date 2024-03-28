bbf62a981133922ee9e3dfda8e6a9d01534b9fa3
Choi-Hyeok99 <gur0709@naver.com>
2024-03-28 15:18:58 +0900

    @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient{
 
     private String url;
@@ -31,12 +34,14 @@ public class NetworkClient{
 
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init ");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
